e49946bad9dd7f8e59f4ca9511cfc5959e1805a2
kumarprashant24 <kumar.prashant@tothenew.com>
2022-07-15 15:57:31 +0530

    fixed peer connection

--- server.js
+++ server.js
@@ -7,41 +7,39 @@ const mongoose = require('mongoose')
 const server = require('http').createServer(app);
 const passport = require('passport');
 const io = require('socket.io')(server, {
-    cors: {
-      origin: "*"
-    }
-  });
-  const cookie = {secret:"secret",resave:true,saveUninitialized:true};
+  cors: {
+    origin: "*"
+  }
+});
+const cookie = { secret: "secret", resave: true, saveUninitialized: true };
 
-  io.on('connection', (socket) =>{
-  
-    socket.on("join_room", (data) => {
-      
-        socket.join(data.room);
-        socket.to(data.room).emit("user-connected",data);
-        socket.on('disconnect',()=>{
-        io.to(data.room).emit("user-disconnected",data);
-        })
-      })
+io.on('connection', (socket) => {
 
-      socket.on("call-ended",(data)=>{
-      
-       io.to(data.room).emit("user-leave",data)
-      })
-      
-   
-  });
+  socket.on("join_room", (data) => {
 
-  app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
+    socket.join(data.room);
+
+    socket.to(data.room).emit("user-connected", data);
+    socket.on('disconnect', () => {
+      io.to(data.room).emit("user-disconnected", data);
     })
-  );
+  })
+  socket.on("call-ended", (data) => {
 
-  app.set('trust proxy', 1);
-  app.use(expressSession({secret:"secret",resave:true,saveUninitialized:true}));
+    io.to(data.room).emit("user-leave", data)
+  })
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+);
+
+app.set('trust proxy', 1);
+app.use(expressSession({ secret: "secret", resave: true, saveUninitialized: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,12 +47,12 @@ app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-  app.use('/api', require('./routes/index'));
-  
-  mongoose.connect('mongodb+srv://prashant24:Prince24@cluster0.2pd6v.mongodb.net/eagle_meet?retryWrites=true&w=majority', (err) => {
+app.use('/api', require('./routes/index'));
+
+mongoose.connect('mongodb+srv://prashant24:Prince24@cluster0.2pd6v.mongodb.net/eagle_meet?retryWrites=true&w=majority', (err) => {
   !err && console.log('connected to database');
   err && console.log(err.message);
 });
 
 
-  server.listen(5000, () => console.log('Server running at port', 5000));
+server.listen(5000, () => console.log('Server running at port', 5000));
