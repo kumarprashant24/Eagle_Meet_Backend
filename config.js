55b8820a62ec7b20f1b612c2ffff027b35ced7ca
kumarprashant24 <kumar.prashant@tothenew.com>
2022-07-26 15:14:30 +0530

    removing check vars

--- config.js
+++ config.js
@@ -9,7 +9,15 @@ if (process.env.NODE_ENV === 'prod') {
   exports.MONGO_URI = process.env.MONGO_URI;
   exports.CLIENT_URL = process.env.CLIENT_URL;
   exports.SERVER_URL = process.env.SERVER_URL;
-  exports.cookie = { secret: "secret", resave: true, saveUninitialized: true }
+  exports.cookie = {
+    secret: 'Session Secret',
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 24 * 60 * 60 * 100,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'none',
+  };
 }
 if (process.env.NODE_ENV === 'dev') {
 
