dc25076e0d9ee1ff81a097b1b4922fc1e906a584
kumarprashant24 <kumar.prashant@tothenew.com>
2022-07-27 13:14:30 +0530

    checking files

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -78,7 +78,7 @@ router.get('/login/success', (req, res) => {
  
     if (req.user) {
         res.send({ success: true, user: req.user });
-    } else res.send({ success: true});
+    } else res.send({ success: false});
 });
 
 router.get('/logout', function(req, res) {
